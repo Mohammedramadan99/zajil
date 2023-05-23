6ed98ddd05bbdb8f62a33f5a6d07b60be28f9891
mowafy001 <wafy123445@gmail.com>
2023-05-23 19:50:36 +0300

    fix: add cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,11 @@
 import Express from 'express';
 import { applyMiddlewaresPost, applyMiddlewaresPre } from './middlewares';
 import { handleErrors } from './middlewares/methods/handleErrors.middleware';
+import cors from 'cors';
 import mainRouter from './routes';
 
 const app = Express();
+app.use(cors());
 app.use(Express.json());
 applyMiddlewaresPre(app);
 app.use(mainRouter);
