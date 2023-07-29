567737cdf5e168df090e2ebbd22d2257ba17aeb3
mohammedramadan99 <lifeisagame0101@gmail.com>
2023-07-30 00:30:56 +0300

    save changes

--- client/src/admin/pages/home/Home.jsx
+++ client/src/admin/pages/home/Home.jsx
@@ -26,9 +26,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { getCards, getCardsCount } from "../../../store/statsSlice";
 
 function Home() {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
   const theme = useTheme();
-  const { cardsCount } = useSelector((state) => state.stats);
+  const {cardsCount} = useSelector(state => state.stats)
   const cards = [
     {
       id: Math.floor(Math.random() * 100000) + 1,
@@ -59,17 +59,58 @@ function Home() {
       icon: <PaymentOutlined />,
     },
   ];
-
+  
+  
   useEffect(() => {
-    dispatch(getCardsCount());
-  }, []);
-
+    
+      dispatch(getCardsCount())
+    
+  }, [dispatch])
+  
   return (
     <Box
       sx={{
         backgroundColor: theme.palette.background.alt,
       }}>
-      game
+      <Grid container spacing={2} p={2}>
+        {/* <Grid xs={6} item>
+          <Card>
+            <CardContent>
+              <Typography variant="h3">Hi, Welcome Back Mohammed!</Typography>
+              <Typography variant="body2" my={1}>
+                You have used the 85% of free plan storage. Please upgrade your
+                plan to get unlimited storage.
+              </Typography>
+              <Button variant="contained" color="primary">
+                upgrade
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid> */}
+
+        <Grid xs={12} item container spacing={2}>
+          {cards.map((item, i) => (
+            <Grid xs={6} item key={item.id}>
+              <CardStats />
+            </Grid>
+          ))}
+        </Grid>
+        <Grid xs={12} md={6} item>
+          <LineChart />
+        </Grid>
+        <Grid xs={12} md={6} item>
+          <SyncLineChart />
+        </Grid>
+        <Grid xs={12} md={6} item>
+          <ShapeBarChart />
+        </Grid>
+        <Grid xs={12} md={6} item>
+          <BarChartNoPadding />
+        </Grid>
+        <Grid xs={12} md={6} item>
+          <OrdersTable data={orders} />
+        </Grid>
+      </Grid>
     </Box>
   );
 }
