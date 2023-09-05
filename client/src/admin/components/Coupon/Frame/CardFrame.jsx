7fd5da4fdd3d17833e089a30fd5e6244678382da
mohammedramadan99 <lifeisagame0101@gmail.com>
2023-09-05 23:51:11 +0300

    Prepare coupon CardFrame component

--- client/src/admin/components/Coupon/Frame/CardFrame.jsx
+++ client/src/admin/components/Coupon/Frame/CardFrame.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
-
+import React from "react";
+import { Box, Grid, Stack, Typography } from "@mui/material";
 function CardFrame() {
   return (
-    <div>CardFrame</div>
-  )
+      <Box>
+          {/* ============ Start Row 1 ============*/}
+          <Grid container>
+              <Grid item>
+                  logo
+              </Grid>
+              <Grid item>
+                  discound 50%
+              </Grid>
+          </Grid>
+          {/* ============ End Row 1 ============*/}
+          {/* ============ Start Row 2 ============*/}
+          <Grid container>
+              <Box>
+                  <Typography> Aid Mubark </Typography>
+              </Box>
+          </Grid>
+          {/* ============ End Row 2 ============*/}
+          {/* ============ Start Row 3 ============*/}
+          <Grid container>
+              <Grid item>
+                  <Typography> Available uses </Typography>
+                  <span>3</span>
+              </Grid>
+              <Grid item>
+                  <Typography> Available uses </Typography>
+                  <span>3</span>
+              </Grid>
+              <Grid item>
+                  <Typography> Available uses </Typography>
+                  <span>3</span>
+              </Grid>
+          </Grid>
+          {/* ============ End Row 3 ============*/}
+          
+    </Box>
+  );
 }
 
-export default CardFrame
+export default CardFrame;
