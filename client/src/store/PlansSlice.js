c4cac857b6996dd744243ef99f4ec4cd2486feae
mohammedramadan99 <lifeisagame0101@gmail.com>
2023-10-09 23:28:08 +0300

    Add plansSlice

---
+++ client/src/store/PlansSlice.js
@@ -0,0 +1,182 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const createPlan = createAsyncThunk(
+  "plan/create",
+  async (actionData, { rejectWithValue, getState }) => {
+    try {
+      const { auth } = getState();
+      const { user } = auth;
+      //   const form = new FormData();
+      //   Object.keys(values).forEach((key) => {
+      //     form.append(key, values[key]);
+      //   });
+
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/plans`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(actionData),
+      });
+      if (!response.ok) {
+        const errorData = await response.json();
+
+        return rejectWithValue({
+          message: errorData?.data.message || "An unknown error occurred.",
+        });
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      return rejectWithValue({
+        message: "An unknown error occurred. Please try again later.",
+      });
+    }
+  }
+);
+export const getPlan = createAsyncThunk(
+  "plan/details",
+  async (id, { rejectWithValue, getState }) => {
+    try {
+      const { auth } = getState();
+      const { user } = auth;
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/plans/${id}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+      if (!response.ok) {
+        const errorData = await response.json();
+
+        return rejectWithValue({
+          message: errorData?.data.message || "An unknown error occurred.",
+        });
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      return rejectWithValue({
+        message: "An unknown error occurred. Please try again later.",
+      });
+    }
+  }
+);
+export const updatePlan = createAsyncThunk(
+  "plan/update",
+  async (actionData, { rejectWithValue, getState }) => {
+    try {
+      const { auth } = getState();
+      const { user } = auth;
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/plans/${actionData.params.id}`,
+        {
+          method: "PATCH",
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(actionData.data),
+        }
+      );
+      if (!response.ok) {
+        const errorData = await response.json();
+
+        return rejectWithValue({
+          message: errorData?.data.message || "An unknown error occurred.",
+        });
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      return rejectWithValue({
+        message: "An unknown error occurred. Please try again later.",
+      });
+    }
+  }
+);
+export const deletePlan = createAsyncThunk(
+  "plan/delete",
+  async (id, { rejectWithValue, getState }) => {
+    try {
+      const { auth } = getState();
+      const { user } = auth;
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/plans/${id}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+      if (!response.ok) {
+        const errorData = await response.json();
+
+        return rejectWithValue({
+          message: errorData?.data.message || "An unknown error occurred.",
+        });
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      return rejectWithValue({
+        message: "An unknown error occurred. Please try again later.",
+      });
+    }
+  }
+);
+
+const initialState = {
+  allPlans: null,
+  planDetails: null,
+  deleted: false,
+  updated: false,
+  loading: false,
+  errors: null,
+};
+const planSlice = createSlice({
+  name: "plans",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.errors = null;
+      state.errorMessage = null;
+      state.successMessage = null;
+      state.errors = null;
+      state.deleted = false;
+      state.updated = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(createPlan.pending, (state, action) => {
+      state.loading = true;
+      state.errors = null;
+    });
+    builder.addCase(createPlan.fulfilled, (state, action) => {
+      state.loading = false;
+      state.errors = null;
+      state.business = action.payload.data;
+    });
+    builder.addCase(createPlan.rejected, (state, action) => {
+      state.loading = false;
+      state.errors = action.payload?.errors;
+      state.user = null;
+      state.errorMessage =
+        action.payload.message ||
+        "An unknown error occurred. Please try again later.";
+    });
+  },
+});
+
+export const { reset } = planSlice.actions;
+
+export default planSlice.reducer;
